df2c10bd1bc7f411c3a00290274ee0abd5664101
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-06-03 01:16:49 +0530

    added print page

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// if(typeof window.scriptExecuted === 'undefined') {
-//     window.scriptExecuted = true;
-//     console.log('script executed');
-// }
+
+
+/* TODO,
+bug fix 1: when double clicking on input, it keeps subtracting forever
+bug fix 2: input values not getting transfered to print-form elements
+*/
+
+
+
 const date = new Date();
 const currMonth = date.getMonth();
 let currYear = parseInt(date.getFullYear());
 let original_bill = undefined;
 let bill = undefined;
-// console.log(original_bill)
+let tax = '0';
+let errors = new Array();
 $(document).ready(
     function () {
         const months = {
@@ -34,28 +40,44 @@ $(document).ready(
     }
 );
 
+if (errors.length != 0) {
+    
+}
+
 function error(ele) {
+    const id = $(ele).prop('id');
     if (/[a-bA-B]/.test($(ele).val())) {
         $(ele).css('border-color', '#CA4E79');
-        $('#calculate').removeClass('print');
+        if (!errors.includes(id)) {
+            errors.push(id);
+            $('.print').css('opacity', '0.5');
+        }
         return true;
     } else {
-        $('#calculate').addClass('print');
-        $(ele).css('border-color', '#A0DEFF');
+        if (errors.includes(id)) {
+            errors.splice(errors.indexOf(id), 1);
+            if (errors.length == 0) {
+                $('.print').css('opacity', '1');
+            }
+        }
+        $(ele).css('border-color', '#EEF5FF');
+        return false;
     }
 }
 
 $('.input-fields').click(function () {
     $(this).css('border-color', '#A0DEFF');
-    if ($(this).val() != "") {
+    if ($(this).val() != "" && !error(this)) {
         bill = bill - $(this).val();
         $('#bal').text(bill);
     }
 });
 
 $('.input-fields').blur(function () {
-    if (!error(this) && $(this).val() != "") {
-        $(this).css('border-color', '#EEF5FF');
+    if (error(this)) {
+        return;
+    }
+    if ($(this).val() != "") {
         let initial = $('#bal').text();
         bill = parseFloat(initial) + parseFloat($(this).val());
         $('#bal').text(bill);
@@ -79,22 +101,55 @@ $('.month').click(function () {
 });
 
 $('.toggle__input').on('change', function () {
+
+    if (bill == undefined || bill == 0) {
+        return;
+    }
+
     if (this.checked) {
         bill = (parseFloat(bill) * 0.18) + parseFloat(bill);
+        tax = '18%';
     } else {
-        console.log(original_bill);
+        tax = '0';
         bill = parseFloat(original_bill);
     }
     $('#bal').text(Math.floor(bill));
 });
 
-$('.print').click(
-    function() {
-        if (bill === undefined || bill == 0) {
-            $('.input-fields').css('border-color', '#CA4E79');
-            return;
-        }
-        alert(bill);
+function printer() {
+    
+    const printForm = () => {
+        const printFrame = $("<iFrame>", {
+            src: 'index.html',
+            title: 'summary',
+            style: 'display: none'
+        }).appendTo('body');
+        
+        printFrame.on('load', function() {
+            const contentWindow = this.contentWindow;
+            contentWindow.focus();
+            contentWindow.print();
+            // contentWindow.remove();
+        })
     }
-)
+
+    printForm();
+
+}
+
+$('#printer').click(
+    function() {
+        if (errors.length == 0 && bill != undefined && bill != 0) {
+
+            $('#rent').text('none');
+            $('#elebill').text('none');
+            $('#oth').text('none');
+            $('#dt').text('none');
+            $('#tax').text(tax);
+            $('#bill').text(bill);
+            printer();
+
+        }
+    }
+);
 
